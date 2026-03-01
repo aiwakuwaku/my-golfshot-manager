d761d2f2c2cce71b260c4ea39096ecef6acb03ec
aiwakuwaku <parplay1872@gmail.com>
2026-03-01 10:18:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-const CACHE_NAME = 'golf-manager-v3';
-const assets = ['./', './index.html', './manifest.json', './icon.svg?v=2'];
+const CACHE_NAME = 'golf-v3';
+const assets = ['./', './index.html', './manifest.json', './icon.svg'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
