9a6f371dffb1808f067aac9a3c30db335c8b5c35
aiwakuwaku <parplay1872@gmail.com>
2026-03-01 11:15:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
 --- START OF FILE sw.js ---
-const CACHE_NAME = 'golf-mgr-v6';
-const assets = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'golf-mgr-v8';
+const assets = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
